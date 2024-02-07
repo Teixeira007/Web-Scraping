a9b04013a7c0af3b8e4fff57c7d64f57c3ed29aa
teixeira007 <viniciusteixeiravini16@gmail.com>
2024-02-06 21:19:48 -0300

    refatoração no códifo - contantes explicitas, função main

--- webScraping.py
+++ webScraping.py
@@ -4,6 +4,11 @@ import re
 import os
 import zipfile
 
+# Constantes explícitas
+BASE_URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+OUTPUT_FOLDER = 'downloads'
+ZIP_FILENAME = 'arquivos_pdfs.zip'
+
 #Função para fazer o download do arquivo recebendo a url e o nome
 def download_file(pdf_url, filename):
     pdf_response = requests.get(pdf_url)
@@ -18,43 +23,44 @@ def zip_file(zip_filename, downloaded_files):
         for file in downloaded_files:
             zip_file.write(file, os.path.basename(file))
 
-#Url que desejo fazer o web scraping
-url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
-#Fazendo uma requisição http do tipo get na url
-response = requests.get(url)
-
-output_folder = 'downloands'
-
-#verificar se existe a pasta downloads, senão, criar uma
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-#Lista que ficarão os nomes dos arquivos a serem baixados
-downloaded_files = []
-
-#Se a requisição foi um sucesso
-if response.status_code == 200:
-    #utilizando o beautifulSoup para  analisar o html da página
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    #Extraindo links de PDF que contém a palavra "anexo"
-    links_pdfs = soup.find_all('a', href=re.compile(r'Anexo.*\.pdf$'))
+def main():
+    #Fazendo uma requisição http do tipo get na url
+    response = requests.get(BASE_URL)
 
 
-    for link in links_pdfs:
-        pdf_url = link.get('href')
+    #verificar se existe a pasta downloads, senão, criar uma
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
 
-        #Extrair o nome do arquivo do url
-        filename = os.path.join(output_folder, os.path.basename(pdf_url))
+    #Lista que ficarão os nomes dos arquivos a serem baixados
+    downloaded_files = []
 
-        #chamando função de download
-        download_file(pdf_url, filename)
+    #Se a requisição foi um sucesso
+    if response.status_code == 200:
+        #utilizando o beautifulSoup para  analisar o html da página
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        #Extraindo links de PDF que contém a palavra "anexo"
+        links_pdfs = soup.find_all('a', href=re.compile(r'Anexo.*\.pdf$'))
+
+
+        for link in links_pdfs:
+            pdf_url = link.get('href')
+
+            #Extrair o nome do arquivo do url
+            filename = os.path.join(OUTPUT_FOLDER, os.path.basename(pdf_url))
+
+            #chamando função de download
+            download_file(pdf_url, filename)
+            
+            downloaded_files.append(filename)
+        #Criar um zip com os arquivos baixados
+        zip_file(ZIP_FILENAME, downloaded_files)
         
-        downloaded_files.append(filename)
-    #Criar um zip com os arquivos baixados
-    zip_filename = 'arquivos_pdfs.zip'
-    zip_file(zip_filename, downloaded_files)
-    
-else:
-    print("Erro ao acessar apágina", response.status_code)
+    else:
+        print("Erro ao acessar apágina", response.status_code)
+
+
+if __name__ == '__main__':
+    main()
